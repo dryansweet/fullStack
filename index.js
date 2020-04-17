7c057005aefc02bd1c9a60171dc6b80615bfad46
D Ryan Sweet <ryansweet@northwesternmutual.com>
2020-04-17 09:55:34 -0500

    removed cors dependency

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+// const cors = require('cors')
 
-app.use(express.json(), cors())
+// app.use(express.json(), cors())
+app.use(express.json())
 
 let notes = [
   {
